7dbcb7ee02226b8b8749dc752b221c3987b42d4c
Pack1e <61186869+Pack1e@users.noreply.github.com>
2020-05-01 12:07:46 +0200

    Methoden zur Umrechnung der Währung

--- myVersionierung/src/Git/Client.java
+++ myVersionierung/src/Git/Client.java
@@ -56,7 +56,26 @@ public class Client {
 		System.out.println("Welchen Betrag möchten Sie umrechnen?");
 		userValue = Benutzereingabe.nextDouble();
 		System.out.println("");
-		System.out.println("Eigegebener Betrag: "+userValue);
+		
+		//Suche in instanziierten Währungen nach eingegebener Währung und rechne sie um
+		if(Euro.validate(userCurrency)) {
+			System.out.println("Eigegebener Betrag ("+Euro.getCurrency()+"): "+userValue);
+			System.out.println("Eigegebener Betrag (Dollar): "+Euro.toDollar(userValue));
+			System.out.println("Eigegebener Betrag (Pfund): "+Euro.toPfund(userValue));
+		}
+		else if(Dollar.validate(userCurrency)) {
+			System.out.println("Eigegebener Betrag ("+Dollar.getCurrency()+"): "+userValue);
+			System.out.println("Eigegebener Betrag (Euro): "+Dollar.toEuro(userValue));
+			System.out.println("Eigegebener Betrag (Pfund): "+Dollar.toPfund(userValue));
+		}
+		else if(Pfund.validate(userCurrency)) {
+			System.out.println("Eigegebener Betrag ("+Pfund.getCurrency()+"): "+userValue);
+			System.out.println("Eigegebener Betrag (Euro): "+Pfund.toEuro(userValue));
+			System.out.println("Eigegebener Betrag (Dollar): "+Pfund.toDollar(userValue));
+		}
+		else {
+			System.out.println("Eingegebene Währung nicht gefunden.");
+		}
 		
 		System.out.println("");
 		
--- myVersionierung/src/Git/Currency.java
+++ myVersionierung/src/Git/Currency.java
@@ -23,27 +23,39 @@ public class Currency {
 	
 	/**
 	 * Konstante zur Umrechnung von Euro in Dollar. 
-	 * {@link} https://www.finanzen.net/waehrungsrechner/euro_us-dollar
+	 * {@link} https://www.finanzen.net/waehrungsrechner/euro_us-dollar (01.05.2020)
 	 */
 	public static final double euro2dollar = 1.0946;
 	
 	/**
-	 * Konstante zur Umrechnung von Euro in Dollar. 
-	 * {@link} https://www.finanzen.net/waehrungsrechner/us-dollar_euro
+	 * Konstante zur Umrechnung von Euro in Pfund. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/euro_britische-pfund (01.05.2020)
+	 */
+	public static final double euro2pfund = 0.8714;
+	
+	/**
+	 * Konstante zur Umrechnung von Dollar in Euro. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/us-dollar_euro (01.05.2020)
 	 */
 	public static final double dollar2euro = 0.9137;
 	
 	/**
-	 * Konstante zur Umrechnung von Euro in Dollar. 
-	 * {@link} https://www.finanzen.net/waehrungsrechner/euro_britische-pfund
+	 * Konstante zur Umrechnung von Dollar in Pfund. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/us-dollar_britische-pfund (01.05.2020)
 	 */
-	public static final double euro2pfund = 0.8698;
+	public static final double dollar2pfund = 0.7965;
 	
 	/**
-	 * Konstante zur Umrechnung von Euro in Dollar. 
-	 * {@link} https://www.finanzen.net/waehrungsrechner/britische-pfund_euro
+	 * Konstante zur Umrechnung von Pfund in Euro. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/britische-pfund_euro (01.05.2020)
 	 */
 	public static final double pfund2euro = 1.1495;
+	
+	/**
+	 * Konstante zur Umrechnung von Pfund in Dollar. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/britische-pfund_us-dollar (01.05.2020)
+	 */
+	public static final double pfund2dollar = 1.2582;
 	
 	/**
 	 * Initialisiere Variable für Name der ausgewählten Währung
@@ -55,7 +67,10 @@ public class Currency {
 	 */
 	private String currencySymbol = null;
 	
-	
+	/**
+	 * Konstruktor konfiguriert die Klasse mit der eingegebenen Währung
+	 * @param curr
+	 */
 	public Currency(String curr) {
 		switch(curr) {
 		case euro: 
@@ -78,6 +93,11 @@ public class Currency {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param input
+	 * @return Bestätigung (true), wenn die Eingabe der aktuellen Währung entspricht.
+	 */
 	public boolean validate(String input) {
 		if(this.currency.equals(input) || this.currencySymbol.equals(input)) {
 			return true;
@@ -85,8 +105,105 @@ public class Currency {
 		return false;
 	}
 	
+	/**
+	 * 
+	 * @return Name der instanziierte Währung
+	 */
 	public String getCurrency() {
 		return this.currency;
 	}
+	
+	/**
+	 * 
+	 * @return Währungssymbol der instanziierten Währung
+	 */
+	public String getCurrencySymbol() {
+		return this.currencySymbol;
+	}
+	
+	/**
+	 * Rechnet die aktuelle Währung in Euro um
+	 * @param value 
+	 * @return Währung in Euro
+	 */
+	public double toEuro(double value) {
+		//Initialisiere Ergebnisvariable
+		double result;
+		
+		//Frage aktuelle Währung ab und führe entsprechende Umrechnung mit Konstanten aus. 
+		//Für 2 Nachkommastellen wird der Wert vor dem Runden mit 100 multipliziert und anschließend wieder durch 100 geteilt
+		switch(this.currency) {
+		case euro:
+			result = value;
+			break;
+		case dollar: 
+			result = (double) Math.round(value*dollar2euro*100)/100;;
+			break;
+		case pfund:
+			result = (double) Math.round(value*pfund2euro*100)/100;
+			break;
+		default:
+			result = 0;
+			break;
+		}
+		return result;
+	}
+	
+	/**
+	 * Rechnet die aktuelle Währung in Dollar um
+	 * @param value 
+	 * @return Währung in Dollar
+	 */
+	public double toDollar(double value) {
+		//Initialisiere Ergebnisvariable
+		double result;
+		
+		//Frage aktuelle Währung ab und führe entsprechende Umrechnung mit Konstanten aus. 
+		//Für 2 Nachkommastellen wird der Wert vor dem Runden mit 100 multipliziert und anschließend wieder durch 100 geteilt
+		switch(this.currency) {
+		case euro:
+			result = (double) Math.round(value*euro2dollar*100)/100;
+			break;
+		case dollar: 
+			result = value;
+			break;
+		case pfund:
+			result = (double) Math.round(value*pfund2dollar*100)/100;
+			break;
+		default:
+			result = 0;
+			break;
+		}
+		return result;
+	}
+	
+	/**
+	 * Rechnet die aktuelle Währung in Pfund um
+	 * @param value 
+	 * @return Währung in Pfund
+	 */
+	public double toPfund(double value) {
+		//Initialisiere Ergebnisvariable
+		double result;
+		
+		//Frage aktuelle Währung ab und führe entsprechende Umrechnung mit Konstanten aus. 
+		//Für 2 Nachkommastellen wird der Wert vor dem Runden mit 100 multipliziert und anschließend wieder durch 100 geteilt
+		switch(this.currency) {
+		case euro:
+			result = (double) Math.round(value*euro2pfund*100)/100;
+			break;
+		case dollar: 
+			result = (double) Math.round(value*dollar2pfund*100)/100;
+			break;
+		case pfund:
+			result = value;
+			break;
+		default:
+			result = 0;
+			break;
+		}
+		return result;
+	}
+	
 	
 }
