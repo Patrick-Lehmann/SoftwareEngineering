d3d186539c4531e40eb82180fcc97d7092e8f012
Pack1e <61186869+Pack1e@users.noreply.github.com>
2020-05-01 10:07:46 +0200

    interaktive Benutzereingabe

--- myVersionierung/src/Git/Client.java
+++ myVersionierung/src/Git/Client.java
@@ -3,6 +3,8 @@
  */
 package Git;
 
+import java.util.Scanner;
+
 /**
  * @author Patrick-Lehmann
  *
@@ -13,13 +15,33 @@ public class Client {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Willkommenstext
 		System.out.println("Willkommen im Währungsrechner!");
 		System.out.println("");
 		
+		//Instanziiere Währungen
 		Currency Euro = new Currency(Currency.euro);
 		Currency Dollar = new Currency(Currency.dollar);
 		Currency Pfund = new Currency(Currency.pfund);
+		
+		System.out.println("");
+		
+		//interaktive Benutzereingabe
+		Scanner Benutzereingabe = new Scanner(System.in);
+		String userCurrency = null;
+		double userValue = 0.0;
+		
+		//Frage Benutzer nach der Währung
+		System.out.println("Welche Währung möchten Sie umrechnen?");
+		userCurrency = Benutzereingabe.next();
+		System.out.println("");
+		System.out.println("Eigegebene Währung: "+userCurrency);
+		
+		//Frage Benutzer nach dem Betrag
+		System.out.println("Welchen Betrag möchten Sie umrechnen?");
+		userValue = Benutzereingabe.nextDouble();
+		System.out.println("");
+		System.out.println("Eigegebener Betrag: "+userValue);
 		
 	}
 
