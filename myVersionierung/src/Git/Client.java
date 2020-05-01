45409da8e80803c40362b2621712b8cd2d2ef626
Pack1e <61186869+Pack1e@users.noreply.github.com>
2020-05-01 09:28:47 +0200

    Willkommenstext

--- myVersionierung/src/Git/Client.java
+++ myVersionierung/src/Git/Client.java
@@ -14,7 +14,7 @@ public class Client {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Willkommen im Währungsrechner!");
 	}
 
 }
