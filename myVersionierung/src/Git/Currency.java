340d208ba1073773e44a3ab4df75381bdf10277d
Pack1e <61186869+Pack1e@users.noreply.github.com>
2020-05-01 09:47:08 +0200

    neue Klasse mit Attributen

---
+++ myVersionierung/src/Git/Currency.java
@@ -0,0 +1,34 @@
+package Git;
+
+/**
+ * @author Patrick-Lehmann
+ *
+ */
+public class Currency {
+	
+	/**
+	 * Konstante zur Umrechnung von Euro in Dollar. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/euro_us-dollar
+	 */
+	public static final double euro2dollar = 1.0946;
+	
+	/**
+	 * Konstante zur Umrechnung von Euro in Dollar. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/us-dollar_euro
+	 */
+	public static final double dollar2euro = 0.9137;
+	
+	/**
+	 * Konstante zur Umrechnung von Euro in Dollar. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/euro_britische-pfund
+	 */
+	public static final double euro2pfund = 0.8698;
+	
+	/**
+	 * Konstante zur Umrechnung von Euro in Dollar. 
+	 * {@link} https://www.finanzen.net/waehrungsrechner/britische-pfund_euro
+	 */
+	public static final double pfund2euro = 1.1495;
+	
+	
+}
