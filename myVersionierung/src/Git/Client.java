7ad8d8a56e004365fe2ce0489d4a02b159f7ccf4
Pack1e <61186869+Pack1e@users.noreply.github.com>
2020-05-01 09:57:09 +0200

    Instanziierung von Währung Euro, Dollar u. Pfund

--- myVersionierung/src/Git/Client.java
+++ myVersionierung/src/Git/Client.java
@@ -15,6 +15,12 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Willkommen im Währungsrechner!");
+		System.out.println("");
+		
+		Currency Euro = new Currency(Currency.euro);
+		Currency Dollar = new Currency(Currency.dollar);
+		Currency Pfund = new Currency(Currency.pfund);
+		
 	}
 
 }
--- myVersionierung/src/Git/Currency.java
+++ myVersionierung/src/Git/Currency.java
@@ -7,6 +7,21 @@ package Git;
 public class Currency {
 	
 	/**
+	 * Konstanter Name für Währung Euro
+	 */
+	public static final String euro = "Euro";
+	
+	/**
+	 * Konstanter Name für Währung Dollar
+	 */
+	public static final String dollar = "Dollar";
+	
+	/**
+	 * Konstanter Name für Währung Pfund
+	 */
+	public static final String pfund = "Pfund";
+	
+	/**
 	 * Konstante zur Umrechnung von Euro in Dollar. 
 	 * {@link} https://www.finanzen.net/waehrungsrechner/euro_us-dollar
 	 */
@@ -30,5 +45,13 @@ public class Currency {
 	 */
 	public static final double pfund2euro = 1.1495;
 	
+	
+	public Currency(String curr) {
+		switch(curr) {
+		case euro: System.out.println("Währung "+euro+" instanziiert..."); break;
+		case dollar: System.out.println("Währung "+dollar+" instanziiert..."); break;
+		case pfund: System.out.println("Währung "+pfund+" instanziiert..."); break;
+		}
+	}
 	
 }
